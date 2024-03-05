6c5e143ec91312013b127dde47bc7e02b43f18b0
Mateusz Goryczka <mateuszgoryczka@o2.pl>
2024-03-05 12:10:19 +0100

    add settings to toolbar and create grid

--- PSMG_CaloriesApp/PSMG_CaloriesApp/App.xaml.cs
+++ PSMG_CaloriesApp/PSMG_CaloriesApp/App.xaml.cs
@@ -10,7 +10,7 @@ namespace PSMG_CaloriesApp
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new NavigationPage(new MainPage());
         }
 
         protected override void OnStart()
